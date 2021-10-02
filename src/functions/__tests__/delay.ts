9da7a35456eb2dc9a08b18ac72f75ffe18222c90
Marius Andra <marius.andra@gmail.com>
2021-10-02 23:28:52 +0200

    add sample test for delay function

---
+++ src/functions/__tests__/delay.ts
@@ -0,0 +1,23 @@
+import { delay as _delay } from '../delay'
+
+describe('functions/delay', () => {
+  const logic = Symbol('stable symbol') as any
+  describe('sync', () => {
+    const delay = _delay.sync!
+    it('returns async operations', () => {
+      const response = delay(logic, 10)
+      expect(response).toEqual([{ logic, operation: 'delay', payload: 10 }])
+    })
+  })
+
+  describe('async', () => {
+    const delay = _delay.async!
+    it('awaits for as long as it says it does', async () => {
+      const start = new Date().valueOf()
+      await delay(logic, 10)
+      const end = new Date().valueOf()
+      expect(end - start).toBeGreaterThanOrEqual(10)
+      expect(end - start).toBeLessThan(20)
+    })
+  })
+})
